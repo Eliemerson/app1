abbca1baa31a06d3c8444d63c9b27475791c6118
Eliemerson <eliemerson_fonseca@hotmail.com>
2018-08-14 11:01:48 -0300

    Atualização nav bar e Corousel

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -8,11 +8,11 @@ import { CalculoComponent } from './calculo/calculo.component';
 import { PodutosComponent } from './podutos/podutos.component';
 
 const  APP_ROUTES: Routes =[
-   {path:'',component: HomeComponent},
+   {path:'',component: HomeComponent},//path: caminho, Component: faz chamada para renderizar um component
    {path:'Cadastro',component:PainelComponent},
    {path:'Login', component:LoginComponent},
    {path:'Calculo', component: CalculoComponent},
    {path:'Produtos', component: PodutosComponent}
 ]
 
-export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
+export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES); // variavel que contem definição das configuração das rotas 
